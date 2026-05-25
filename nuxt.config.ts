e83fbe530de283b0407d5e8f6fba9d4243d9a897
KimBlazter <blasster35@gmail.com>
2026-05-25 16:28:13 +0200

    feat: update nuxt configuration and add favicon and add vercel speed insights module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,13 +8,38 @@ export default defineNuxtConfig({
     vite: {
         plugins: [tailwindcss()],
     },
-    modules: ['@nuxt/ui'],
+    modules: ['@nuxt/ui', '@vercel/speed-insights'],
     typescript: {
         nodeTsConfig: {
             compilerOptions: {
                 types: ['@types/node'],
             },
             include: ['../scripts/**/*.ts'],
+        },
+    },
+    app: {
+        head: {
+            title: 'PokeSpore',
+            meta: [
+                {
+                    name: 'description',
+                    content: 'Quiz Pokémon Français communautaire',
+                },
+                {
+                    name: 'viewport',
+                    content: 'width=device-width, initial-scale=1',
+                },
+            ],
+            htmlAttrs: {
+                lang: 'fr',
+            },
+            link: [
+                {
+                    rel: 'icon',
+                    type: 'image/x-icon',
+                    href: '/favicon.ico',
+                },
+            ],
         },
     },
 })
